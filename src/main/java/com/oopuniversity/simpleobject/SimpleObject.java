e6a43df60cd333a27a38f511f65f53f9988ebd06
oopuniversity@jdluke.com <G1tHub00P>
2016-06-11 07:55:49 -0400

    Initial commit

---
+++ src/main/java/com/oopuniversity/simpleobject/SimpleObject.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2016. OOP University.
+ *
+ * License is granted to use this software for educational purposes only.
+ */
+
+package com.oopuniversity.simpleobject;
+
+/**
+ * A class that can do nothing but be instantiated
+ * Created by OOP University on 6/8/2016.
+ */
+public class SimpleObject {
+}
---
+++ src/main/java/com/oopuniversity/simpleobject/SimpleObjectApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016. OOP University.
+ *
+ * License is granted to use this software for educational purposes only.
+ */
+
+package com.oopuniversity.simpleobject;
+
+/**
+ * Created by jd on 6/8/2016.
+ */
+public class SimpleObjectApp {
+    public static void main(String [] args) {
+        SimpleObject simpleObject = new SimpleObject();
+        System.out.println("Yay!");
+    }
+}
